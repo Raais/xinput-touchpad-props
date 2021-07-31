0a5473e90eb2a78d871c1b303614e374c992c63f
Raais <64605172+Raais@users.noreply.github.com>
2021-07-31 17:56:39 +0500

    Add files via upload

---
+++ touchpad-props.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+tpadid=$(xinput list | grep 'Touchpad' | awk {'print$6'} | awk {'print substr($0,4)'})
+teprop=$(xinput list-props $tpadid | grep 'Tapping Enabled' | head -1 | awk {'print $4'} | cut -d "(" -f2 | cut -d ")" -f1)
+dwtprop=$(xinput list-props $tpadid | grep 'Disable While Typing' | head -1 | awk {'print $6'} | cut -d "(" -f2 | cut -d")" -f1)
+
+xinput set-prop $tpadid $teprop 1
+xinput set-prop $tpadid $dwtprop 0
