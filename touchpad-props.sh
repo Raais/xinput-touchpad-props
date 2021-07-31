851cc9e51d332209b4d1fcafaa1e0d0289a0a96d
Raais <64605172+Raais@users.noreply.github.com>
2021-07-31 17:59:13 +0500

    Update touchpad-props.sh

--- touchpad-props.sh
+++ touchpad-props.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#make executable: sudo chmod +x touchpad-props.sh
+#script might need changes depending on your setup
+
 tpadid=$(xinput list | grep 'Touchpad' | awk {'print$6'} | awk {'print substr($0,4)'})
 teprop=$(xinput list-props $tpadid | grep 'Tapping Enabled' | head -1 | awk {'print $4'} | cut -d "(" -f2 | cut -d ")" -f1)
 dwtprop=$(xinput list-props $tpadid | grep 'Disable While Typing' | head -1 | awk {'print $6'} | cut -d "(" -f2 | cut -d")" -f1)
